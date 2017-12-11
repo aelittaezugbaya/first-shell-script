90c04643c2fa0c75bc04d27d84c88cf61e761236
Aelitta Ezugbaya <aelittae@gmail.com>
2017-12-11 15:53:15 +0200

    script write result on file

--- script.sh
+++ script.sh
@@ -2,11 +2,16 @@
 numNodes=$1
 i=0
 array
+>file.txt
+echo 'new' > file.txt
 while [ $i -lt $numNodes ]
 do
 	echo $i
-	array[$i]=$i
+	j=$((i+1))
+	echo $j >> file.txt
+	array[$i]=$j
 	i=$((i+1))
+	
 done
 echo 'Number of nodes is' $numNodes
 echo "First Method: ${array[*]}"
