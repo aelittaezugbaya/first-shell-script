4228b2e6cae6dcb4a50532c49cf64453eadd9dee
Aelitta Ezugbaya <aelittae@gmail.com>
2017-12-11 14:37:36 +0200

    added whilr loop

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 numNodes=$1
+i=0
+array
+while [ $i -lt $numNodes ]
+do
+	echo $i
+	array[$i]=$i
+	i=$((i+1))
+done
 echo 'Number of nodes is' $numNodes
+echo "First Method: ${array[*]}"
